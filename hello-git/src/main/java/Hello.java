76d02089d170a0337e6179ee327c642284a829ed
legend <gendway@163.com>
2018-10-19 15:59:58 +0800

    原始版本

---
+++ hello-git/src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * @Description:
+ * @Date:created in 15:49 2018/10/19
+ */
+public class Hello {
+
+
+    public static void main(String[] args) {
+        System.out.println ("你好实际");
+        System.out.println ("do");
+        System.out.println ("ljf");
+    }
+}
